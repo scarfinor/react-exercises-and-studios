ad87d9b13c7bbd7f302b3f3dbab6a38325ca0067
scarfinor <scarfinor@gmail.com>
2024-08-14 23:18:21 -0500

    git commit completed BookList ChoresList MovieList

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,21 @@
-export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+import React from "react";
+import classes from "./BookList.module.css";
 
-   return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
-      </div>      
-   );
+export default function BookList() {
+  let pageTitle = "Latest Book Releases";
+  let book1 =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0YbUT1Gx0kdUAtIxX_TVD9QG2Dxmdl0LXXJTEVMYi4JxbpOcTEIAdxitG_Q&s=5";
+  let book2 =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUNyOsJ25_weLzWagjrevvALmSMzO-CZxuj8NtbR9AerFyRAEmD47MHqOyKbTTNGTXwGqLk2A&s=0";
+  let book3 =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnkkaJzYbpWTyHxA5rA2ORuFYOMrr57-jLjFnaEPcf5UrF18tYUL0Y9avxcg&s=5";
+
+  return (
+    <div className={classes.div}>
+      <h3 className={classes.bookHeading}>{pageTitle}</h3>
+      <img src={book1} alt="Animal Farm by George Orwell" />
+      <img src={book2} alt="Don Quixote by Miguel de Cervantes" />
+      <img src={book3} alt="Dracula by Bram Stoker" />
+    </div>
+  );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,15 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from "react";
+import classes from "./ChoresList.module.css";
+
+export default function ChoresList() {
+  return (
+    <div>
+      <h3 className={classes.choresHeading}>Today's Chores</h3>
+      <ul>
+        <li className={classes.choresText}>Empty dishwasher</li>
+        <li className={classes.choresText}>Complete prep work</li>
+        <li className={classes.choresText}>Buy groceries</li>
+      </ul>
+    </div>
+  );
 }
