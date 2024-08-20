feb37139d0a0865f04d2973363dbe791ab8942d7
scarfinor <scarfinor@gmail.com>
2024-08-19 20:31:21 -0500

    git commit added Ingredients.jsx Ingredients.module.css updated App.jsx

--- part1/studio/src/App.jsx
+++ part1/studio/src/App.jsx
@@ -1,12 +1,14 @@
 //import { useState } from "react";
 import "./App.css";
 import RecipeDescription from "./components/Description";
+import RecipeIngredients from "./components/Ingredients";
 
 function App() {
   return (
     <>
       <div className="App">
         <RecipeDescription />
+        <RecipeIngredients />
       </div>
     </>
   );
