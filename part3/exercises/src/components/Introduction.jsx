8d812ced220077bc2106c72677ca268c5a9226d1
scarfinor <scarfinor@gmail.com>
2024-08-21 21:52:13 -0500

    git commit added Introduction.jsx

---
+++ part3/exercises/src/components/Introduction.jsx
@@ -0,0 +1,14 @@
+export default function HobbyIntroduction() {
+  return (
+    <div>
+      <h2>3 Fun Facts About Call Of Duty</h2>
+      <ul>
+        <li>World wide competitive scene</li>
+        <li>The CDL has 12 teams</li>
+        <li>
+          The top ranked team on average earns 1 million dollars each year
+        </li>
+      </ul>
+    </div>
+  );
+}
